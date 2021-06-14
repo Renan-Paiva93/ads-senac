a0f3650f24f6c59ee929a810b041dfdc0140fde6
Renan Paiva <71081313+Renan-Paiva93@users.noreply.github.com>
2021-06-14 19:41:55 -0300

    subir

---
+++ pacote-downloads/projetointegradori/Jogo1.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetointegradori;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * 
+ */
+public class Jogo1 {
+
+    //variavel contadora para controle de pontuação
+    int cont = 0;
+
+    Perguntas1 pergunta = new Perguntas1();
+    Respostas1 resposta = new Respostas1();
+    Random rand = new Random();
+    Scanner scan = new Scanner(System.in);
+
+    //metodo responsavel por popular vetor de perguntas na ordem "aleatoria"
+    public void ordenarPerguntas() {
+        int ordem = rand.nextInt(4) + 1;
+        pergunta.popularPerguntas(ordem);
+        resposta.popularRespostas(ordem);
+    }
+
+    //metodo responsável por executar as ações do jogo
+    public void jogar() {
+
+        
+        String resp;
+        ordenarPerguntas();
+                
+        for (int i = 0; i < 7; i++) {
+            if (cont == 3) {
+                System.out.println("\nVOCÊ CHEGOU NO BOSS\n");
+                boss();
+                break;
+            } else if (cont == -3) {
+                System.out.println("\nVOCÊ FOI ELIMINADO\n");
+                cont = 0;
+                break;
+            } else if (i == 6 && cont < 3 && cont > -3) {
+                System.out.println("Tente novamente, vou te dar uma colher de chá...");
+                ordenarPerguntas();
+                i = 0;
+                cont = 0;
+            }
+
+            System.out.println(pergunta.perguntas[i]);
+            resp = scan.next().toUpperCase();
+
+            if (resp.equals(resposta.respostas[i])) {
+                System.out.println("Hm... pensou rápido!! Está correto.");
+                cont++;
+            } else {
+                System.out.println("Não foi dessa vez, meu caro... HEHEHEHEHE");
+                cont--;
+            }
+        }
+    }
+
+    public void boss() {
+        int scoreBoss = 0;
+        String respUser;
+        for(int i = 0; i<pergunta.perguntasBoss.length; i++){
+            System.out.println(pergunta.getPerguntasPopuladas()[i]);
+            respUser = scan.next().toUpperCase();
+            
+             if (respUser.equals(resposta.getRespostasPopuladas()[i])) {
+                System.out.println("Ora ora... você está pronto para combater o crime em Arkham City, você salvou o Batman!!");
+                scoreBoss++;
+            } else {
+                System.out.println("\nVOCÊ FOI ELIMINADO\n");
+                cont = 0;
+                break;
+            }
+        }
+    }
+}
---
+++ pacote-downloads/projetointegradori/Perguntas1.java
@@ -0,0 +1,82 @@
+
+package projetointegradori;
+
+/**
+ *
+ * @author Arthur / Renan
+ */
+public class Perguntas1 {
+    
+    String pergunta1 = "Seus pais morreram após uma apresentação de circo.";
+    String pergunta2 = "O mordomo da família, Alfred Pennyworth era seu principal suporte na luta contra o crime.";
+    String pergunta3 = "Seu principal poder é super força e agilidade extrema.";
+    String pergunta4 = "Seu modo de luta não se adequa à morte.";
+    String pergunta5 = "Seu principal veículo de passeio é uma Ferrari.";
+    String pergunta6 = "Seu principal aliado contra o crime era Flash.";
+    String pergunta7 = "A primeira a pessoa a consolar o Bruce após a perda dos seus pais foi Jim Gordon.";
+    
+    //vetor de perguntas de 7 posições
+    public String[] perguntas = new String[7];
+    
+    //vetor perguntas Boss
+    public String[] perguntasBoss = new String[3];
+    
+    //metodo responsavel por popular a o vetor na ordem recebida por parametro
+    public void popularPerguntas(int ordem){
+        
+        if(ordem == 1){
+            this.perguntas[0] = pergunta1;
+            this.perguntas[1] = pergunta2;
+            this.perguntas[2] = pergunta3;
+            this.perguntas[3] = pergunta4;
+            this.perguntas[4] = pergunta5;
+            this.perguntas[5] = pergunta6;
+            this.perguntas[6] = pergunta7;
+        }
+        
+        if(ordem == 2){
+            this.perguntas[0] = pergunta6;
+            this.perguntas[1] = pergunta7;
+            this.perguntas[2] = pergunta3;
+            this.perguntas[3] = pergunta4;
+            this.perguntas[4] = pergunta5;
+            this.perguntas[5] = pergunta1;
+            this.perguntas[6] = pergunta2;
+        }
+        
+        if(ordem == 3){
+            this.perguntas[0] = pergunta5;
+            this.perguntas[1] = pergunta1;
+            this.perguntas[2] = pergunta7;
+            this.perguntas[3] = pergunta4;
+            this.perguntas[4] = pergunta3;
+            this.perguntas[5] = pergunta2;
+            this.perguntas[6] = pergunta6;
+       }
+        
+        if(ordem == 4){
+            this.perguntas[0] = pergunta2;
+            this.perguntas[1] = pergunta4;
+            this.perguntas[2] = pergunta6;
+            this.perguntas[3] = pergunta1;
+            this.perguntas[4] = pergunta7;
+            this.perguntas[5] = pergunta3;
+            this.perguntas[6] = pergunta5;
+        }
+    }
+    
+    public String[] getPerguntasPopuladas(){
+        this.perguntasBoss[0] = "Coringa ganha suas cicatrizes após ser questionado\n"
+        + "por seu pai do por quê estar tão sério ao encontrar sua mãe sendo espancada pelo mesmo";
+        
+        this.perguntasBoss[1] = "Segundo boatos, o ator Heath Ledger, que foi encontrado morto em seu apartamento em Nova York\n"
+        + " no dia 22 de janeiro de 2008, sofreu uma parada cardíaca causada por abuso de remédios prescritos. Ledger já estava usando\n"
+        + "medicamentos para dormir, uma vez que nas filmagens de Batman, o Cavaleiro das Trevas, seu emocional estava em constante teste.\n"
+        + "Isto nos leva a cogitar que talvez a carga que o ator extraiu do Coringa para si mesmo possa ter o 'enlouquecido' e influenciado em seu descontrole.";
+        
+        this.perguntasBoss[2] = "No final da história Batman encontra o Coringa no topo do prédio e o mata de forma brutal que assusta\n"
+        + " até o comissário Gordon, fazendo ele ser procurado pela polícia.";
+        
+        return this.perguntasBoss;        
+    }
+}
