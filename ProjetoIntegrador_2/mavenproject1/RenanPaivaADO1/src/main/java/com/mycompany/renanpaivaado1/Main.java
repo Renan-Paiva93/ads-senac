4116ccb5d4db76bb9ebdd7cd3c983d6779d69f31
Renan Paiva <71081313+Renan-Paiva93@users.noreply.github.com>
2021-09-09 17:44:00 -0300

    atual ado1

---
+++ ProjetoIntegrador_2/mavenproject1/RenanPaivaADO1/src/main/java/com/mycompany/renanpaivaado1/Main.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.renanpaivaado1;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ ProjetoIntegrador_2/mavenproject1/RenanPaivaADO1/src/main/java/com/mycompany/renanpaivaado1/Raposa.java
@@ -0,0 +1,28 @@
+
+package com.mycompany.renanpaivaado1;
+
+
+public class Raposa {
+    
+    //Atributos
+    private String nome;
+    private String cor;
+    private String sexo;
+    private int velocidade;
+    
+    //construtor vazio
+    public Raposa(){
+        
+    }
+    
+    // parametros como construir
+     public Raposa(String corAnimal) {
+       this.cor = corAnimal;
+    }
+
+    //Ações - metodos
+    public void andar(){}
+    
+    public void dormi(){}
+
+}
