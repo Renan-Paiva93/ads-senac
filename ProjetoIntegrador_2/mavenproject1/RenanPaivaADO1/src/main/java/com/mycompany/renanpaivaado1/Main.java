dcc2d2fe34fcd7736220931a5ae576df0558c0bb
Renan Paiva <71081313+Renan-Paiva93@users.noreply.github.com>
2021-09-22 17:43:38 -0300

    aula 5 PI

--- ProjetoIntegrador_2/mavenproject1/RenanPaivaADO1/src/main/java/com/mycompany/renanpaivaado1/Main.java
+++ ProjetoIntegrador_2/mavenproject1/RenanPaivaADO1/src/main/java/com/mycompany/renanpaivaado1/Main.java
@@ -1,11 +1,19 @@
 
 package com.mycompany.renanpaivaado1;
 
-
 public class Main {
 
     public static void main(String[] args) {
         
+        Raposa novoAnimal = new Raposa();
+        
+        novoAnimal.cor = "laranja";
+        novoAnimal.sexo = "Masculino";
+        novoAnimal.nome = "Raposa";
+        
+        novoAnimal.parar();
+        novoAnimal.andar();
+        novoAnimal.movimentar();
+
     }
-    
 }
--- ProjetoIntegrador_2/mavenproject1/RenanPaivaADO1/src/main/java/com/mycompany/renanpaivaado1/Raposa.java
+++ ProjetoIntegrador_2/mavenproject1/RenanPaivaADO1/src/main/java/com/mycompany/renanpaivaado1/Raposa.java
@@ -5,24 +5,59 @@ package com.mycompany.renanpaivaado1;
 public class Raposa {
     
     //Atributos
-    private String nome;
-    private String cor;
-    private String sexo;
-    private int velocidade;
+     String nome;
+     String cor;
+     String sexo;
+     boolean movimento;
+     
     
     //construtor vazio
     public Raposa(){
+      cor = "laranja";
+    }
+    
+    
+    
+    //Métodos - Ações (o que ele pode fazer)
+     void movimentar(){
+        if (this.movimento == true){
+            System.out.println("Não pode se movimentar");
+        } else
+            System.out.println("Estou andando");
+    }
+    
+    public void andar(){
+       this.movimento = true;
+    }
+    
+    public void parar(){
+        this.movimento = false;
+    }
+
+    public Raposa(String sexo, int velocidade) {
+        this.sexo = sexo;
+        this.movimento = movimento;
+    }
+
+   
+
+    //Metodos - Acessores
+    public void setMovimento(boolean movimento) {
+        this.movimento = movimento;
+    }
+
+    public String getNome() {
         
-    }
-    
-    // parametros como construir
-     public Raposa(String corAnimal) {
-       this.cor = corAnimal;
+        return nome;
     }
 
-    //Ações - metodos
-    public void andar(){}
-    
-    public void dormi(){}
+    public String getCor() {
+        return cor;
+    }
 
+    public String getSexo() {
+        return sexo;
+    }
+    
+  
 }
