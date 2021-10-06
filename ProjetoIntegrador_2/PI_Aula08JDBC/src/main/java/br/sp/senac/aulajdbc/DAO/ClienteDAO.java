18f3e6dfbc63b802295c8ef5a8e273afce9e8453
Renan Paiva <71081313+Renan-Paiva93@users.noreply.github.com>
2021-10-06 16:42:46 -0300

    aula 8 e loja

---
+++ ProjetoIntegrador_2/PI_Aula08JDBC/src/main/java/br/sp/senac/aulajdbc/DAO/ClienteDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.sp.senac.aulajdbc.DAO;
+
+/**
+ *
+ * @author renan.paiva
+ */
+public class ClienteDAO {
+    
+     public static void salvar(String nome, String CPF) {
+        
+    }
+    
+}
---
+++ ProjetoIntegrador_2/PI_Aula08JDBC/src/main/java/com/mycompany/pi_aula08jdbc/Main.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.pi_aula08jdbc;
+
+/**
+ *
+ * @author renan.paiva
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
