732be6f139cbba69e6fcab9328927969b3a64d20
Renan Paiva <71081313+Renan-Paiva93@users.noreply.github.com>
2021-10-25 21:45:31 -0300

    ultima atusl

---
+++ ProjetoIntegrador_2/PI_Aula09_MVC/src/main/java/com/mycompany/pi_aula09_mvc/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pi_aula09_mvc;
+
+import br.sp.senac.aulamvc.view.NotaFiscalView;
+
+/**
+ *
+ * @author Dell
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        NotaFiscalView telaInicial = new NotaFiscalView();
+        telaInicial.setVisible(true);
+        
+        
+    }
+    
+}
