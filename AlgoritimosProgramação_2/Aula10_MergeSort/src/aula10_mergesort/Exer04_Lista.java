5d8b11dc33d065bc1c46be3fdd9883bfac73c88c
Renan Paiva <71081313+Renan-Paiva93@users.noreply.github.com>
2021-10-26 09:23:11 -0300

    Create exercicio 4

---
+++ AlgoritimosProgramação_2/Aula10_MergeSort/src/aula10_mergesort/Exer04_Lista.java
@@ -0,0 +1,23 @@
+/*
+
+4) A INTERCALAÇÃO é o processo utilizado para construir uma vetor ordenado
+crescentemente, de tamanho n+m, a partir de dois vetores já ordenados de tamanhos
+n e m. Por exemplo, a partir das sequências abaixo:
+
+Exemplo:
+A[] = { 1, 3, 6, 7} e B[] = {2, 4, 5}, o novo vetor C é feito a partir de A[] e B[]:
+C = { 1, 2, 3, 4, 5, 6, 7}
+
+Escreva uma função que recebe dois vetores inteiros A[] e B[], em seguida, a sua
+função calcula e retorna o resultado da INTERCALAÇÃO, ou seja, o vetor C[].
+
+ */
+package aula10_mergesort;
+
+/**
+ *
+ * @author renan.paiva
+ */
+public class Exer04_Lista {
+    
+}
