085ed3a4c267a4017bc9af196f159aad243e06ed
Renan Paiva <71081313+Renan-Paiva93@users.noreply.github.com>
2021-11-05 15:59:56 -0300

    trabalho ADO2

--- AlgoritimosProgramação_2/Trabalho_02/src/trabalho_02/Exercicio_04.java
+++ AlgoritimosProgramação_2/Trabalho_02/src/trabalho_02/Exercicio_04.java
@@ -1,14 +1,33 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Dada uma string, criar uma função recursiva (sem laços) que compute o número de caracteres ’x’ na string.
+Exemplos:
+contarX("xxhixx")  4
+contarX("xhixhix")  3
+contarX("hi")  0
+
+Dica: utilize os métodos length() e substring() da classe String.
+
  */
 package trabalho_02;
-
 /**
  *
  * @author renan.paiva
  */
 public class Exercicio_04 {
+    public static void main(String[] args) {
+        int totalX = totalDeX("xxxttxxtxxtxtttxx", 'x');
+        System.out.println("Número " + totalX);
+      
+    }
+    public static int totalDeX(String string, char x){
+       
+        if(string.equals("")){
+            return 0;
+        }else if (string.charAt(0) != x){
+            return totalDeX(string.substring(1), x);
+        }else{
+            return totalDeX(string.substring(1), x) + 1;
+        }
+    }
     
 }
--- AlgoritimosProgramação_2/Trabalho_02/src/trabalho_02/Exercicio_05.java
+++ AlgoritimosProgramação_2/Trabalho_02/src/trabalho_02/Exercicio_05.java
@@ -1,7 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+5) Temos triângulos feito de blocos. A linha superior tem 1 bloco, a próxima linha abaixo tem 2 blocos, a
+próxima linha tem 3 blocos e assim por diante. Criar uma função recursiva que calcule (sem loops ou multiplicação)
+o número total de blocos em um triângulo com o número especificado de linhas.
+Exemplos:
+triangulo(0)  0
+triangulo(1)  1
+triangulo(2)  3
  */
 package trabalho_02;
 
@@ -10,5 +14,17 @@ package trabalho_02;
  * @author renan.paiva
  */
 public class Exercicio_05 {
+    public static void main(String[] args) {
+        int linha = blocos(3);
+        System.out.println("Total de Blocos " + linha);
+      
+    }
+    public static int blocos(int n){
+        if(n == 0){
+           return 0;
+        }else{
+           return n + blocos(n - 1);
+        }
+    }
     
 }
