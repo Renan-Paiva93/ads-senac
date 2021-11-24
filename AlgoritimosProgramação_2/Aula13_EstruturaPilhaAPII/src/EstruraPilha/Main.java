08cb35a4cf6939199206b3fef782087e2ebdd6e6
Renan Paiva <71081313+Renan-Paiva93@users.noreply.github.com>
2021-11-24 20:12:00 -0300

    pilha

--- AlgoritimosProgramação_2/Aula13_EstruturaPilhaAPII/src/EstruraPilha/Main.java
+++ AlgoritimosProgramação_2/Aula13_EstruturaPilhaAPII/src/EstruraPilha/Main.java
@@ -1,10 +1,20 @@
 
 package EstruraPilha;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Dell
  */
 public class Main {
+    
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Pilha p = new Pilha();
+        
+        
+        
+    }
     
 }
