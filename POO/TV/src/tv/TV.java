8c00a2225573ab429f7d6040410333de8491642a
Renan Paiva <71081313+Renan-Paiva93@users.noreply.github.com>
2022-03-07 22:01:40 -0300

    atual senac

---
+++ POO/TV/src/tv/TV.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tv;
+
+/**
+ *
+ * @author Dell
+ */
+public class TV {
+
+    private int volume;
+    private int canal;
+    private boolean ligada;
+
+    /**
+     * @return the volume
+     */
+    public int getVolume() {
+        return volume;
+    }
+
+    /**
+     * @param volume the volume to set
+     */
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    /**
+     * @return the canal
+     */
+    public int getCanal() {
+        return canal;
+    }
+
+    /**
+     * @param canal the canal to set
+     */
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+
+    /**
+     * @return the ligada
+     */
+    public boolean isLigada() {
+        return ligada;
+    }
+
+    /**
+     * @param ligada the ligada to set
+     */
+    public void setLigada(boolean ligada) {
+        this.ligada = ligada;
+    }
+    
+    
+    
+}
