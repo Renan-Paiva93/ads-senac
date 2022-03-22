b8fa26bf0a8657e88e9b40c99a66c3c00edb8618
Renan Paiva <71081313+Renan-Paiva93@users.noreply.github.com>
2022-03-22 20:40:04 -0300

    Create Ex2PesqBinaria.java

---
+++ Estruturas de Dados/Exemplo_SelSort_01/src/main/java/Aulas/Ex2PesqBinaria.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aulas;
+
+/**
+ *
+ * @author Dell
+ */
+public class Ex2PesqBinaria {
+    
+    static final int TAMANHO=5;
+    static Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        int vet[] = new int[TAMANHO];
+        
+    }
+    
+}
