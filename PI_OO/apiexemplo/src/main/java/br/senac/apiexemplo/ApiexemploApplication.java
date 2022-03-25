f36d7f57ba91f01eaa8339b6aada17f0b5ee7369
Renan Paiva <71081313+Renan-Paiva93@users.noreply.github.com>
2022-03-24 22:47:15 -0300

    API

---
+++ PI_OO/apiexemplo/src/main/java/br/senac/apiexemplo/ApiexemploApplication.java
@@ -0,0 +1,13 @@
+package br.senac.apiexemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiexemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiexemploApplication.class, args);
+	}
+
+}
