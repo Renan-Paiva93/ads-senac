3fcf3764635e289e92f826b6097ed50ff6c26eef
Renan Paiva <71081313+Renan-Paiva93@users.noreply.github.com>
2022-08-29 20:51:10 -0300

    exemplo javascript

---
+++ Semestre 4/desenv-sistema-web/exemplos-js/src/main/java/br/senac/tads/dsw/exemplos/ExemplosJsApplication.java
@@ -0,0 +1,13 @@
+package br.senac.tads.dsw.exemplos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplosJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemplosJsApplication.class, args);
+	}
+
+}
---
+++ Semestre 4/desenv-sistema-web/exemplos-js/src/test/java/br/senac/tads/dsw/exemplos/ExemplosJsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.tads.dsw.exemplos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplosJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
